e808dff93704aa26895da9a7153c0464a1c95e1d
LaPampa <kozdrabartek@gmail.com>
2024-06-05 22:32:14 +0200

    Changed hosts field in hardening playbook from devservers to all

--- vagrant_vars.rb
+++ vagrant_vars.rb
@@ -1,9 +1,9 @@
 # ip_addresses.rb
 
 # configure IP addresses accordingly
-IP_APP_MAIN = "0.0.0.0"
-IP_APP_SEC = "0.0.0.0"
-IP_DEV = "0.0.0.0"
-IP_DB = "0.0.0.0"
-IP_MAIL = "0.0.0.0"
+IP_APP_MAIN = "10.10.10.100"
+IP_APP_SEC = "10.10.10.101"
+IP_DEV = "10.10.10.10"
+IP_DB = "10.10.10.102"
+IP_MAIL = "10.10.10.103"
 
