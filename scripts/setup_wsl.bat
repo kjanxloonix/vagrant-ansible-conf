ba368baa2a9cec4e36fcd7a8081f035d4d6b660a
kjanxloonix <kja@onet.eu>
2024-06-05 18:49:40 +0200

    Fixed path in batch script

--- scripts/setup_wsl.bat
+++ scripts/setup_wsl.bat
@@ -2,7 +2,7 @@
 
 rem Add a file to WSL environment
 wsl.exe pwd
-wsl.exe -u root cp "./templates/wsl.conf.temp" "/etc/wsl.conf"
+wsl.exe -u root cp "./scripts/templates/wsl.conf.temp" "/etc/wsl.conf"
 
 rem Terminate WSL environment
 wsl.exe --terminate %1
